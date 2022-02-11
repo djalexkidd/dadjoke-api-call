e8183bc6e0059bf7d11e5c4da8c86786384c2dce
djalexkidd <alexpyr@outlook.fr>
2022-02-11 10:07:26 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const APICALL = "https://icanhazdadjoke.com/"
+const joke = document.querySelector('.joke')
+const button = document.querySelector('.getanother')
+
+// Création d'une fonction asynchrone
+async function anotherJoke() {
+    const reponse = await fetch(`${APICALL}`, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const data = await reponse.json()
+    console.log(data)
+
+    creationBlague(data)
+}
+
+// Affichage de la blague
+function creationBlague(output) {
+    joke.innerText = `${output.joke}`
+}
+
+// Bouton pour générer une autre blague
+button.addEventListener('click', (e) => {
+    anotherJoke()
+})
+
+anotherJoke()
